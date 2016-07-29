876dd6a5281ad025055dfd1d4e88502c954fe6bc
Stiletto <stilett0@users.noreply.github.com>
2016-07-28 23:46:16 -0400

    big update (nw)
    big update (nw)

--- links.php
+++ links.php
@@ -8,28 +8,155 @@ $title = 'MAME | Resources';
 
 	<center><h1 class="page-header">Resources</h1></center>
 
+	<h2>Information</h2>
+	<u1>
+		<li><a href="http://www.mamedev.org/about.html">About</a><li>
+		<li><a href="http://www.mamedev.org/history.html">History</a><li>
+		<li><a href="http://www.mamedev.org/legal.html">Legal</a><li>
+		<li><a href="http://www.mamedev.org/contact.html">Contact</a><li>
+	</u1>
+	<h2>Downloads></h2>
+	<u1>
+		<li><a href="http://www.mamedev.org/release.html">Latest Release</a><li>
+		<li><a href="http://www.mamedev.org/oldrel.html">Previous Releases</a><li>
+	</u1>
+	<h2>Documentation</h2>
+	<u1>
+		<li><a href="http://docs.mamedev.org/">MAME Documentation</a><li>
+		<li><a href="http://wiki.mamedev.org/">MAME Development Wiki</a><li>
+		<li><a href="http://wiki.mamedev.org/index.php?title=Frequently_Asked_Questions">MAME FAQ</a><li>
+		<li><a href="http://wiki.mamedev.org/index.php?title=How_MAME_Works">MAME Developer Docs</a><li>
+		<li><a href="http://www.mess.org/mess/howto" target="blank">MESS User's Manual</a><li>
+	</u1>
 	<h2>Individual Developers' Sites</h2>
 	<p>
 	Please visit the <a href="http://wiki.mamedev.org/index.php/Developer_WIPs">Developer WIPs</a>
-	list on our <a href="http://wiki.mamedev.org/index.php/Main_Page">Documentation Wiki</a> for an
+	list on our <a href="http://wiki.mamedev.org/">Documentation Wiki</a> for an
 	up-to-date list of developer blogs, WIP pages, and to-do lists.
 	</p>
 	<h2>Sources for ROM Images</h2>
 	<ul>
 		<li><a href="http://www.mamedev.org/roms" target="blank">mamedev.org</a> &mdash; a few owners have granted permission to use their ROMs for non-commercial use</li>
-		<li><a href="http://www.gaelco.com/english/pages/hablando/frhablan.htm" target="blank">Gaelco</a> &mdash; has released the ROMs for their game <b>World Rally</b> for all to enjoy!</li>
-		<li><a href="http://www.hanaho.com/" target="blank">Hanaho Games</a> &mdash; includes licensed Capcom ROMs with its Hotrod joystick</li>
+		<li><a href="http://web.archive.org/web/20160402171815/http://www.gaelco.com/english/pages/hablando/frhablan.htm" target="blank">Gaelco</a> &mdash; has released the ROMs for their game <b>World Rally</b> for all to enjoy!</li>
 	</ul>
+	<h2>Compiling and Source</h2>
+	<ul>
+		<li><a href="http://mamedev.org/tools/" target="blank">Tools for building MAME on Windows</a></li>
+		<li><a href="https://github.com/mamedev/mame/" target="blank">MAME's official repo on Github</a></li>
+		<li><a href="http://git.redump.net/mame/" target="blank">Redump.net's MAME/MESS GIT Repository Browser</a></li>
+		<li><a href="http://mame.dorando.at/svn/" target="blank">MAME Git History at Dorando.</a></li>
+	</u1>
+	<h2>Extra Files for MAME</h2>
+	<u1>
+		<li><a href="http://www.progettoemma.net/?catlist" target="blank">Catlist</a></li>
+		<li><a href="http://ledblinky.net/Download.htm" target="blank">Colors.ini (LEDBlinky)</a></li>
+		<li><a href="https://sites.google.com/site/procyonsjj/home/command-dat" target="blank">Command.dat</a></li>
+		<li><a href="http://controls.arcadecontrols.com/" target="blank">Controls.dat</a></li>
+		<li><a href="http://www.arcade-history.com/index.php?page=download" target="blank">History.dat</a></li>
+		<li><a href="http://www.progettosnaps.net/gameinit/" target="blank">GameInit.dat</a></li>
+		<li><a href="http://mrdo.mameworld.info/mame_artwork.php" target="blank">MAME Artwork</a></li>
+		<li><a href="http://samples.mameworld.info/" target="blank">MAME Samples</a></li>
+		<li><a href="http://mameinfo.mameworld.info/" target="blank">MAMEInfo.dat</a></li>
+		<li><a href="http://www.mess.org/artwork" target="blank">MESS Artwork</a></li>
+		<li><a href="http://www.progettosnaps.net/messinfo" target="blank">MESSINFO.dat</a></li>
+		<li><a href="http://nplayers.arcadebelgium.be/" target="blank">NPlayers</a></li>		
+		<li><a href="http://www.progettosnaps.net/" target="blank">Progetto-Snaps</a></li>
+		<li><a href="http://cheat.retrogames.com/" target="blank">Pugsy's Cheats</a></li>
+		<li><a href="http://www.arcadehits.net/mamescore/home.php?show=files" target="blank">Story.dat</a></li>
+		<li><a href="http://www.progettoemma.net/mess/extra.html" target="blank">SysInfo.dat</a></li>
+		<li><a href="http://www.progettosnaps.net/command/" target="blank">Unofficial Command.dat</a></li>
+		<li><a href="http://highscore.mameworld.info/" target="blank">Unofficial Hiscore.dat (official comes with MAME)</a></li>
+	</u1>
+	<h2>Frontends</h2>
+	<p>
+	Please visit <a href="http://mameworld.info/" target="blank">MAMEWorld</a> or the <a href="http://www.mess.org/mess/howto#frontends" target="blank">
+	MESS User's Manual</a> for a list of these.
+	</p>
+	<h2>Social Media</h2>
+	<u1>
+		<li><a href="https://www.facebook.com/mamedev.org/" target="blank">MAME on Facebook</a></li>
+		<li><a href="http://mamedev.imgur.com/" target="blank">MAME on imgur</a></li>
+		<li><a href="https://www.instagram.com/mamedev_org/" target="blank">MAME on Instagram</a></li>
+		<li><a href="https://twitter.com/mamedev_org" target="blank">MAME on Twitter</a></li>
+	</u1>
+	<h2>Community and Support</h2>
+	<u1>
+		<li><a href="http://www.mametesters.org/" target="blank">MAME Testers</a></li>
+		<li><a href="http://forum.mamedev.org/" target="blank">MAME Official Forums</a></li>
+		<li><a href="http://www.mamedev.org/irc.html" target="blank">Official MAME Chat on IRC</a></li>
+		<li><a href="http://forums.bannister.org/ubbthreads.php?ubb=postlist&Board=1" target="blank">MAME Forum at Emuversal (Bannister.org)</a></li>
+		<li><a href="http://www.mameworld.info/ubbthreads/ubbthreads.php?Cat=" target="blank">MAMEWorld Forums</a></li>
+		<li><a href="http://forum.arcadecontrols.com/" target="blank">Build Your Own Arcade Controls Forum</a></li>
+		<li><a href="http://www.mameitalia.net/" target="blank">MAME Italia (ITA)</a></li>
+		<li><a href="http://www.arcadeitalia.net/" target="blank">Arcade Italia (ITA)</a></li>
+		<li><a href="http://www.arcadezentrum.com/forum/index.php" target="blank">Arcadezentrum (GER)</a></li>
+		<li><a href="http://www.arcadeinfo.de/" target="blank">Arcadeinfo.de (GER)</a></li>
+		<li><a href="http://www.gamoover.net" target="blank">Gamoover (FRA)</a></li>
+		<li><a href="http://forums.arcade-museum.com/" target="blank">KLOV Forums (ENG)</a></li>
+	</u1>
 	<h2>Other MAME-focused Sites</h2>
 	<ul>
+		<li><a href="http://adb.arcadeitalia.net/" target="blank">Arcade Database (ADB)</a><li>
+		<li><a href="http://mahjonginmame.com/" target="blank">Mahjong in MAME</a><li>
+		<li><a href="http://replay.marpirc.net/" target="blank">MAME Action Replay Page (MARP)</a><li>
+		<li><a href="http://mameui.info/Bench.htm" target="blank">MAME Benchmarks</a><li>
+		<li><a href="http://e2j.net/" target="blank">MAME E2J (JPN)</a><li>
+		<li><a href="http://www.mamedb.com/" target="blank">mamedb.com</a></li>
 		<li><a href="http://www.mameworld.info/" target="blank">MAMEWorld</a></li>
-		<li><a href="http://www.mametesters.org/" target="blank">MAME Testers</a></li>
-		<li><a href="http://www.mameworld.net/maws" target="blank">MAWS</a></li>
-		<li><a href="http://unmamed.mame.net/" target="blank">UnMAMEd Games Database</a></li>
+		<li><a href="http://www.mess.org/" target="blank">MESS</a><li>
+		<li><a href="http://nonmame.retrogames.com/" target="blank">NonMAME</a></li>
+		<li><a href="http://www.progettoemma.net/" target="blank">Progetto EMMA</a></li>
+		<li><a href="http://www.progettoemma.net/mess/" target="blank">ProjectMESS - MESS Software Lists Resource<a></li>
+		<li><a href="http://mamedev.emulab.it/undumped/" target="blank">Undumped Wiki</a></li>
+		<li><a href="http://unmamed.mameworld.info/" target="blank">UnMAMEd Games Database</a></li>
 	</ul>
+	<h2>Unofficial Forks and Ports</h2>
+	<u1>
+		<li><a href="http://www.advancemame.it/" target="blank">AdvanceMAME</a></li>
+		<li><a href="http://hbmame.1emulation.com/" target="blank">HBMAME</a></li>
+		<li><a href="http://mameui.info/" target="blank">MAMEUI</a></li>
+		<li><a href="http://messui.1emulation.com/" target="blank">MESSUI</a></li>
+		<li><a href="http://wolfmame.marpirc.net/" target="blank">WolfMAME</a></li>
+	</u1>
+	<h2>General Emulation Sites</h2>
+	<ul>
+		<li><a href="http://caesar.logiqx.com/" target="blank">CAESAR</a></li>
+		<li><a href="http://www.emudreams.pl/" target="blank">Emu#Dreams (PL)</a></li>
+		<li><a href="http://www.emu-france.com/" target="blank">Emu-France (FRA)</a></li>
+		<li><a href="http://www.emulatronia.com/" target="blank">Emulatronia (ESP)</a></li>
+		<li><a href="http://www.emucamp.com/" target="blank">Emulation Camp</a></li>
+		<li><a href="http://www.emuunlim.com/" target="blank">EmuUnlim</a></li>
+		<li><a href="http://ngemu.com/" target="blank">NGEmu</a></li>		
+		<li><a href="http://www.retrogames.com/" target="blank">Retrogames</a></li>
+		<li><a href="http://www.zophar.net/" target="blank">Zophar's Domain</a></li>
+	</u1>
+	<h2>General Arcade Sites</h2>
+	<ul>
+		<li><a href="http://www.mameworld.info/mameinfo/movies/index.html" target="blank">Arcade At The Movies</a><li>
+		<li><a href="http://www.arcadecollecting.com/" target="blank">ArcadeCollecting.com</a></li>
+		<li><a href="http://arcadeheroes.com/" target="blank">Arcade Heroes</a></li>
+		<li><a href="http://www.arcadeotaku.com/" target="blank">Arcade Otaku</a></li>
+		<li><a href="http://www.atariage.com/" target="blank">AtariAge</a></li>
+		<li><a href="http://www.cinemarcade.com/" target="blank">CinemArcade</a><li>
+		<li><a href="http://www.coinop.org/" target="blank">Coinop.org</a><li>
+		<li><a href="http://discrete.mameworld.info/" target="blank">Discrete Logistics</a><li>
+		<li><a href="http://www.dragons-lair-project.com/" target="blank">Dragon's Lair Project</a><li>
+		<li><a href="http://www.flyerfever.com/" target="blank">Flyer Fever</a><li>
+		<li><a href="http://www.arcade-museum.com/" target="blank">International Arcade Museum (KLOV)</a><li>
+		<li><a href="http://www.ipdb.org/" target="blank">Internet Pinball Database (IPDB)</a></li>
+		<li><a href="http://www.tilt.it/deb/index-en.html" target="blank">Italian Coin-op Videogames Zone (ICVZ)</a><li>
+		<li><a href="http://www.recreativas.org/" target="blank">Recreativas Database</a></li>
+		<li><a href="http://www.system16.com/" target="blank">System 16</a></li>
+		<li><a href="http://strategywiki.org/wiki/Category:MAME" target="blank">StrategyWiki</a></li>
+		<li><a href="http://flyers.arcade-museum.com/" target="blank">The Arcade Flyer Archive</a></li>
+		<li><a href="https://tcrf.net/" target="blank">The Cutting Room Floor</a></li>
+		<li><a href="http://www.twingalaxies.com/" target="blank">Twin Galaxies</a></li>
+		<li><a href="http://www.arcade-museum.com/vaps/" target="blank">VAPS<a></li>
+	</ul>
+	
 
 	<br/><br/>
 </div>
 <!-- /.container -->
 
-<?php require($_SERVER['DOCUMENT_ROOT'] . '/_include/html/footer.html'); ?>					
+<?php require($_SERVER['DOCUMENT_ROOT'] . '/_include/html/footer.html'); ?>
