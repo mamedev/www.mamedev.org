5c68f538bb7b0843536c3d60e369b6674d9da2b0
Vas Crabb <vas@vastheman.com>
2018-05-30 12:55:28 +1000

    bump some dates

--- legal.php
+++ legal.php
@@ -14,7 +14,7 @@ $title = 'MAME | Legal Information';
 
 					<pre>
 MAME
-Copyright (C) 1997-2017  MAMEDev and contributors
+Copyright (C) 1997-2018  MAMEDev and contributors
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
