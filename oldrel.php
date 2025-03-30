744b32473e632e778b91f132e367ee620c422df2
Vas Crabb <vas@vastheman.com>
2025-03-31 03:48:18 +1100

    MAME 0.276

--- oldrel.php
+++ oldrel.php
@@ -84,6 +84,13 @@ function ancient_mirror_directory($version,  $title = NULL)
 				<th width="23%">Whatsnew</th>
 			</tr>
 			<tr>
+				<td class="date"><?php release_mirror_directory('275'); ?></td>
+				<td class="date">26 Feb 2025</td>
+				<td class="link"><?php release_mirror_file('275', 'mame0275s.exe'); ?></td>
+				<td class="link"><?php release_mirror_file('275', 'mame0275b_64bit.exe'); ?></td>
+				<td class="link"><a href="releases/whatsnew_0275.txt">whatsnew_0275.txt</a></td>
+			</tr>
+			<tr>
 				<td class="date"><?php release_mirror_directory('274'); ?></td>
 				<td class="date">31 Jan 2025</td>
 				<td class="link"><?php release_mirror_file('274', 'mame0274s.exe'); ?></td>
